301dcbfbf531d25e2ca44f68c65e6371fd321fbe
Iago López Galeiras <iago@kinvolk.io>
2018-04-12 17:18:38 +0200

    Change back flatcar-cloudinit to coreos-cloudinit
    There's no need to rename this since coreos-cloudinit is just the name of
    the project.

--- platform/api/packet/api.go
+++ platform/api/packet/api.go
@@ -386,7 +386,7 @@ RequiredBy=multi-user.target
 				},
 				ignition.File{
 					Filesystem: "root",
-					Path:       "/root/bin/flatcar-cloudinit",
+					Path:       "/root/bin/coreos-cloudinit",
 					Contents: ignition.FileContents{
 						Source: ignition.Url{
 							Scheme: "data",
