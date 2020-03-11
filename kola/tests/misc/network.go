2efea455effbb5e219c3d6b5dc8babdfbb46443a
Thilo Fromm <thilo@kinvolk.io>
2020-03-11 17:43:36 +0100

    kola network.go: restore original systemd-networkd names
    Signed-off-by: Thilo Fromm <thilo@kinvolk.io>

--- kola/tests/misc/network.go
+++ kola/tests/misc/network.go
@@ -132,10 +132,10 @@ NextProcess:
 
 func NetworkListeners(c cluster.TestCluster) {
 	expectedListeners := []listener{
-		{"tcp", "22", "systemd"},          // ssh
-		{"tcp", "10010", "containerd"},    // containerd
-		{"udp", "68", "systemd-network"},  // dhcp6-client
-		{"udp", "546", "systemd-network"}, // bootpc
+		{"tcp", "22", "systemd"},           // ssh
+		{"tcp", "10010", "containerd"},     // containerd
+		{"udp", "68", "systemd-networkd"},  // dhcp6-client
+		{"udp", "546", "systemd-networkd"}, // bootpc
 	}
 	checkList := func() error {
 		return checkListeners(c, expectedListeners)
