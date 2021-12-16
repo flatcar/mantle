59a8b397ddfdcc6c33f415e5c5f5891cc2c6393f
Mathieu Tortuyaux <mtortuyaux@microsoft.com>
2021-12-16 18:54:10 +0100

    platform/machine/em/console: handle SSH clients
    we can now open, keep a trace and delete properly a SSH client.
    
    When the instance is created, its SSH client will be stored and closed
    when the device is marked as available.
    
    Signed-off-by: Mathieu Tortuyaux <mtortuyaux@microsoft.com>

--- platform/machine/packet/console.go
+++ platform/machine/packet/console.go
@@ -16,6 +16,7 @@ package packet
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 
 	"golang.org/x/crypto/ssh"
@@ -30,7 +31,25 @@ type console struct {
 }
 
 func (c *console) SSHClient(ip, user string) (*ssh.Client, error) {
-	return c.pc.UserSSHClient(ip, user)
+	client, err := c.pc.UserSSHClient(ip, user)
+	if err != nil {
+		return nil, fmt.Errorf("getting SSH client: %w", err)
+	}
+
+	c.ssh = client
+	return client, nil
+}
+
+func (c *console) CloseSSH() error {
+	if c.ssh == nil {
+		return nil
+	}
+
+	if err := c.ssh.Close(); err != nil {
+		return fmt.Errorf("closing SSH client: %w", err)
+	}
+
+	return nil
 }
 
 func (c *console) Write(p []byte) (int, error) {
