73a12cf2ce514d58b82f561b4fd66b014c131b16
Mathieu Tortuyaux <mtortuyaux@microsoft.com>
2022-03-03 15:28:41 +0100

    test/ignition: add symlink test
    this test creates a symlink via ignition and assert it's correct.
    
    Signed-off-by: Mathieu Tortuyaux <mtortuyaux@microsoft.com>

---
+++ kola/tests/ignition/symlink.go
@@ -0,0 +1,47 @@
+// Copyright The Mantle Authors
+// Copyright 2020 Red Hat
+// SPDX-License-Identifier: Apache-2.0
+package ignition
+
+import (
+	"github.com/coreos/go-semver/semver"
+	"github.com/flatcar-linux/mantle/kola/cluster"
+	"github.com/flatcar-linux/mantle/kola/register"
+	"github.com/flatcar-linux/mantle/platform/conf"
+)
+
+func init() {
+	register.Register(&register.Test{
+		Name:        "cl.ignition.symlink",
+		Run:         writeAbsoluteSymlink,
+		ClusterSize: 1,
+		UserData: conf.Ignition(`{
+		  "ignition": {
+		      "version": "3.0.0"
+		  },
+		  "storage": {
+		      "links": [
+		          {
+		              "group": {
+		                  "name": "core"
+		              },
+		              "overwrite": true,
+		              "path": "/etc/localtime",
+		              "user": {
+		                  "name": "core"
+		              },
+		              "hard": false,
+		              "target": "/usr/share/zoneinfo/Europe/Zurich"
+		          }
+		      ]
+		  }
+	      }`),
+		MinVersion: semver.Version{Major: 3185},
+	})
+}
+
+func writeAbsoluteSymlink(c cluster.TestCluster) {
+	m := c.Machines()[0]
+
+	c.AssertCmdOutputContains(m, "readlink /etc/localtime", "/usr/share/zoneinfo/Europe/Zurich")
+}
