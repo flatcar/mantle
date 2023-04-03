c72fc530e0148d16e8d437d9dd14cf4bb7648bd8
Krzesimir Nowak <knowak@microsoft.com>
2023-04-03 18:50:16 +0200

    kola/tests/systemd: New sysext test
    It downloads the developer container, builds a sysext image to test
    OEM, changes OEMID to test and verifies if the sysext image is
    activated after a reboot.
    
    Co-authored-by: Kai Lüke <pothos@users.noreply.github.com>

--- kola/tests/systemd/sysext.go
+++ kola/tests/systemd/sysext.go
@@ -4,14 +4,266 @@
 package systemd
 
 import (
+	"bytes"
+	"encoding/base64"
+	"errors"
 	"fmt"
+	"os"
 	"strings"
+	"text/template"
+	"time"
+	"unicode"
 
 	"github.com/coreos/go-semver/semver"
 	"github.com/flatcar/mantle/kola"
 	"github.com/flatcar/mantle/kola/cluster"
 	"github.com/flatcar/mantle/kola/register"
+	"github.com/flatcar/mantle/platform"
 	"github.com/flatcar/mantle/platform/conf"
+	"github.com/flatcar/mantle/platform/local"
+	"github.com/flatcar/mantle/platform/machine/qemu"
+	"github.com/flatcar/mantle/platform/machine/unprivqemu"
+	"github.com/flatcar/mantle/util"
+)
+
+// BEGIN FOR UTILS
+func trimLeftSpace(contents string) string {
+	return strings.TrimLeftFunc(contents, unicode.IsSpace)
+}
+
+// END FOR UTILS
+
+type (
+	downloadScriptParameters struct {
+		ImageDirectoryURLTemplate string
+	}
+
+	configTemplateParameters struct {
+		DownloadScriptBase64Contents     string
+		DevContainerScriptBase64Contents string
+		MainScriptBase64Contents         string
+		CheckScriptBase64Contents        string
+	}
+)
+
+var (
+	// BEGIN FOR UTILS
+	downloadScriptTemplate = trimLeftSpace(`
+#!/bin/bash
+
+set -x
+
+set -euo pipefail
+
+output_bin="${1}"
+
+function process_template() {
+        local template="${1}"; shift
+        local arch="${1}"; shift
+        local version="${1}"; shift
+        local result="${template}"
+
+        result="${result//@ARCH@/${arch}}"
+        result="${result//@VERSION@/${version}}"
+
+        echo "${result}"
+}
+
+source /usr/share/flatcar/release
+
+ARCH="${FLATCAR_RELEASE_BOARD/-usr/}"
+VERSION="${FLATCAR_RELEASE_VERSION}"
+IMAGE_URL=$(process_template '{{ .ImageDirectoryURLTemplate }}/flatcar_developer_container.bin.bz2' "${ARCH}" "${VERSION}")
+
+echo "Fetching developer container from ${IMAGE_URL}"
+# Stolen from copy_from_buildcache in ci_automation_common.sh. Not
+# using --output-dir option as this seems to be quite a new addition
+# and curl on older version of Flatcar does not understand it.
+curl --fail --silent --show-error --location --retry-delay 1 --retry 60 \
+        --retry-connrefused --retry-max-time 60 --connect-timeout 20 \
+        --remote-name "${IMAGE_URL}"
+
+bzip2cat=bzcat
+if command -v lbzcat; then
+        bzip2cat=lbzcat
+fi
+
+# The image file takes over 6Gb after normal unpacking, but a lot of
+# it is just zeros. Use cp --sparse=always to avoid unnecessary disk
+# space waste. Especially that we may not have 6Gb of disk space
+# available.
+cp --sparse=always <("${bzip2cat}" flatcar_developer_container.bin.bz2) "${output_bin}"
+`)
+	// END FOR UTILS
+
+	devContainerScript = trimLeftSpace(`
+#!/bin/bash
+
+set -x
+
+set -euo pipefail
+
+version=$(source /etc/os-release; echo "${VERSION}")
+version_id=$(source /etc/os-release; echo "${VERSION_ID}")
+board=$(source /usr/share/flatcar/release; echo "${FLATCAR_RELEASE_BOARD}")
+
+mkdir -p /work/sysext_rootfs/usr/share/flatcar-sysext-kola-test
+echo "${version_id}" >/work/sysext_rootfs/usr/share/flatcar-sysext-kola-test/file
+mkdir -p /work/sysext_rootfs/usr/lib/extension-release.d
+sysext_arch=x86-64
+if [[ "${board}" = 'arm64-usr' ]]; then sysext_arch=arm64; fi
+metadata=(
+    'ID=flatcar'
+    "VERSION_ID=${version_id}"
+    "ARCHITECTURE=${sysext_arch}"
+)
+metadata_file=/work/sysext_rootfs/usr/lib/extension-release.d/extension-release.oem-test
+printf '%s\n' "${metadata[@]}" >"${metadata_file}"
+mksquashfs /work/sysext_rootfs "/work/oem-test-${version}.raw" -all-root
+`)
+
+	mainScript = trimLeftSpace(`
+#!/bin/bash
+
+set -x
+
+set -euo pipefail
+
+/home/core/download-script.sh flatcar_developer_container.bin
+
+# This is where the built sysext will be stored
+workdir="${PWD}/dev-container-workdir-${RANDOM}"
+mkdir -p "${workdir}"
+
+sudo systemd-nspawn \
+        --console=pipe \
+        --bind-ro=/home/core/dev-container-script.sh \
+        --bind="${workdir}:/work" \
+        --image=flatcar_developer_container.bin \
+        --machine=flatcar-developer-container \
+        /bin/bash /home/core/dev-container-script.sh
+
+version=$(source /etc/os-release; echo "${VERSION}")
+sysext_file="${workdir}/oem-test-${version}.raw"
+
+if [[ ! -e "${sysext_file}" ]]; then
+        echo "Expected ${sysext_file} to exist, contents of workdir:"
+        ls -la "${workdir}"
+        exit 1
+fi
+
+# Rebrand our image from "qemu" to "test".
+if [[ ! -e /oem/oem-release ]]; then
+        # This is the regular case, on the generic image used for kola
+        # QEMU tests there is no OEM setup
+        printf '%s\n' 'ID=test' 'VERSION_ID=1.0.0' 'NAME=testing stuff' | sudo tee /oem/oem-release >/dev/null
+else
+        # This only works when the OEM setup is optional, e.g., with
+        # the QEMU OEM image
+        sudo sed -i'' -e 's/^ID=.*/ID=test/' /oem/oem-release
+fi
+sudo mkdir -p /oem/sysext
+sudo mv "${sysext_file}" /oem/sysext
+sudo touch /oem/sysext/active-oem-test
+# We keep /var/log to keep journald logs in case something goes wrong.
+sudo flatcar-reset --keep-machine-id --keep-paths /var/log
+`)
+
+	checkScript = trimLeftSpace(`
+#!/bin/bash
+
+set -x
+
+set -euo pipefail
+
+list_out=$(systemd-sysext list --json=pretty)
+status_out=$(systemd-sysext status --json=pretty)
+printf 'sysext list:\n%s\nsysext status:\n%s\n' "${list_out}" "${status_out}"
+
+list_oem_test=$(jq '.[] | select(.name == "oem-test")' <<<"${list_out}")
+
+if [[ -z "${list_oem_test}" ]]; then
+        echo "oem-test image is not listed"
+        exit 1
+fi
+
+oem_test_type=$(jq --raw-output '.type' <<<"${list_oem_test}")
+if [[ "${oem_test_type}" != 'raw' ]]; then
+        echo "oem test image type should be 'raw', is '${oem_test_type}'"
+        exit 1
+fi
+
+oem_test_path=$(jq --raw-output '.path' <<<"${list_oem_test}")
+if [[ "${oem_test_path}" != '/etc/extensions/oem-test.raw' ]]; then
+        echo "oem test image path should be '/etc/extensions/oem-test.raw', is '${oem_test_path}'"
+        exit 1
+fi
+
+status_usr=$(jq '.[] | select(.hierarchy == "/usr")' <<<"${status_out}")
+if [[ -z "${status_usr}" ]]; then
+        echo "no sysext hierarchy for /usr?"
+        exit 1
+fi
+
+status_usr_extensions_oem_test=$(jq --raw-output '.extensions[] | select(. == "oem-test")' <<<"${status_usr}")
+if [[ "${status_usr_extensions_oem_test}" != 'oem-test' ]]; then
+        echo "oem-test sysext is not active on /usr"
+        exit 1
+fi
+
+f=/usr/share/flatcar-sysext-kola-test/file
+if [[ ! -e "${f}" ]]; then
+        echo "Missing file from sysext"
+        exit 1
+fi
+got=$(cat "${f}")
+ex=$(source /etc/os-release; echo "${VERSION_ID}")
+if [[ "${got}" != "${ex}" ]]; then
+        echo "Bad content of sysext file (got '${got}', expected '${ex}')"
+        exit 1
+fi
+`)
+
+	butaneTemplate = trimLeftSpace(`
+variant: flatcar
+version: 1.0.0
+storage:
+  files:
+    - path: /home/core/download-script.sh
+      mode: 0755
+      contents:
+        source: "data:text/plain;base64,{{ .DownloadScriptBase64Contents }}"
+      user:
+        name: core
+      group:
+        name: core
+    - path: /home/core/dev-container-script.sh
+      mode: 0755
+      contents:
+        source: "data:text/plain;base64,{{ .DevContainerScriptBase64Contents }}"
+      user:
+        name: core
+      group:
+        name: core
+    - path: /home/core/main-script.sh
+      mode: 0755
+      contents:
+        source: "data:text/plain;base64,{{ .MainScriptBase64Contents }}"
+      user:
+        name: core
+      group:
+        name: core
+    - path: /oem/check-script.sh
+      # set overwrite to true as flatcar-reset will not remove it
+      overwrite: true
+      mode: 0755
+      contents:
+        source: "data:text/plain;base64,{{ .CheckScriptBase64Contents }}"
+      user:
+        name: core
+      group:
+        name: core
+`)
 )
 
 func init() {
@@ -50,7 +302,18 @@ func init() {
     - path: /etc/extensions/docker-flatcar
     - path: /etc/extensions/containerd-flatcar`),
 	})
-
+	register.Register(&register.Test{
+		Name:        "systemd.sysext.custom-oem",
+		Run:         checkSysextCustomOEM,
+		ClusterSize: 0,
+		Distros:     []string{"cl"},
+		// This test is uses its own OEM files and shouldn't run on other platforms
+		Platforms:  []string{"qemu", "qemu-unpriv"},
+		MinVersion: semver.Version{Major: 3605},
+		NativeFuncs: map[string]func() error{
+			"Http": Serve,
+		},
+	})
 }
 
 func checkHelper(c cluster.TestCluster) {
@@ -98,3 +361,121 @@ func checkSysextCustomDocker(c cluster.TestCluster) {
 	// We should now still be able to use Docker
 	_ = c.MustSSH(c.Machines()[0], cmdWorking)
 }
+
+func checkSysextCustomOEM(c cluster.TestCluster) {
+	// BEGIN COPIED STUFF
+	devcontainerURL := kola.DevcontainerURL
+	if kola.DevcontainerFile != "" {
+		// This URL is deterministic as it runs on the started machine.
+		devcontainerURL = "http://localhost:8080"
+	}
+	// END COPIED STUFF
+
+	userdata, err := prepareUserData(devcontainerURL)
+	if err != nil {
+		c.Fatalf("preparing user data failed: %v", err)
+	}
+	// BEGIN COPIED STUFF
+	machine, err := newMachineWithLargeDisk(c, userdata)
+	if err != nil {
+		c.Fatalf("creating a machine failed: %v", err)
+	}
+
+	if kola.DevcontainerFile != "" {
+		configureHTTPServer(c, machine)
+	}
+	// END COPIED STUFF
+
+	if _, err := c.SSH(machine, "/home/core/main-script.sh"); err != nil {
+		c.Fatalf("main script failed: %v", err)
+	}
+	if err := machine.Reboot(); err != nil {
+		c.Fatalf("could not reboot: %v", err)
+	}
+	if _, err := c.SSH(machine, "/oem/check-script.sh"); err != nil {
+		c.Fatalf("check script failed: %v", err)
+	}
+}
+
+func prepareUserData(devContainerURL string) (*conf.UserData, error) {
+	scriptParameters := downloadScriptParameters{
+		ImageDirectoryURLTemplate: devContainerURL,
+	}
+	downloadScript, err := executeTemplate(downloadScriptTemplate, "download script", scriptParameters)
+	if err != nil {
+		return nil, err
+	}
+	downloadScriptBase64 := base64.StdEncoding.EncodeToString(([]byte)(downloadScript))
+	mainScriptBase64 := base64.StdEncoding.EncodeToString(([]byte)(mainScript))
+	devContainerScriptBase64 := base64.StdEncoding.EncodeToString(([]byte)(devContainerScript))
+	checkScriptBase64 := base64.StdEncoding.EncodeToString(([]byte)(checkScript))
+	configParameters := configTemplateParameters{
+		DownloadScriptBase64Contents:     downloadScriptBase64,
+		DevContainerScriptBase64Contents: devContainerScriptBase64,
+		MainScriptBase64Contents:         mainScriptBase64,
+		CheckScriptBase64Contents:        checkScriptBase64,
+	}
+	config, err := executeTemplate(butaneTemplate, "butane config", configParameters)
+	if err != nil {
+		return nil, err
+	}
+	return conf.Butane(config), nil
+}
+
+// BEGIN COPIED STUFF
+func executeTemplate(contents, name string, parameters any) (string, error) {
+	tmpl, err := template.New(name).Parse(contents)
+	if err != nil {
+		return "", fmt.Errorf("parsing %s as a template failed: %w", name, err)
+	}
+	buf := bytes.Buffer{}
+	if err := tmpl.Execute(&buf, parameters); err != nil {
+		return "", fmt.Errorf("executing %s template failed: %w", name, err)
+	}
+	return buf.String(), nil
+}
+
+func newMachineWithLargeDisk(c cluster.TestCluster, userData *conf.UserData) (platform.Machine, error) {
+	options := platform.MachineOptions{
+		ExtraPrimaryDiskSize: "5G",
+	}
+	switch pc := c.Cluster.(type) {
+	case *qemu.Cluster:
+		return pc.NewMachineWithOptions(userData, options)
+	case *unprivqemu.Cluster:
+		return pc.NewMachineWithOptions(userData, options)
+	}
+	return nil, errors.New("unknown cluster type, this test should only be running on qemu or qemu-unpriv platforms")
+}
+
+func Serve() error {
+	httpServer := local.SimpleHTTP{}
+	return httpServer.Serve()
+}
+
+func configureHTTPServer(c cluster.TestCluster, srv platform.Machine) {
+	// manually copy Kolet on the host, as the initial size cluster is 0.
+	kola.ScpKolet(c, strings.SplitN(kola.QEMUOptions.Board, "-", 2)[0])
+
+	in, err := os.Open(kola.DevcontainerFile)
+	if err != nil {
+		c.Fatalf("opening dev container file: %v", err)
+	}
+
+	defer in.Close()
+
+	if err := platform.InstallFile(in, srv, "/var/www/flatcar_developer_container.bin.bz2"); err != nil {
+		c.Fatalf("copying dev container to HTTP server: %v", err)
+	}
+
+	c.MustSSH(srv, fmt.Sprintf("sudo systemd-run --quiet ./kolet run %s Http", c.H.Name()))
+
+	if err := util.WaitUntilReady(60*time.Second, 5*time.Second, func() (bool, error) {
+		_, _, err := srv.SSH(fmt.Sprintf("curl %s:8080", srv.PrivateIP()))
+		return err == nil, nil
+	}); err != nil {
+		c.Fatal("timed out waiting for http server to become active")
+	}
+}
+
+// END COPIED STUFF
