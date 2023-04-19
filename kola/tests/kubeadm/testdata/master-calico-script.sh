90d59ef54ec89fba5c779859b82313b63a89566b
Krzesimir Nowak <knowak@microsoft.com>
2023-04-19 17:16:52 +0200

    kola/test/kubeadm: Fix registry parameter
    The docs say that this parameter needs to end with a slash.

--- kola/tests/kubeadm/testdata/master-calico-script.sh
+++ kola/tests/kubeadm/testdata/master-calico-script.sh
@@ -83,7 +83,7 @@ metadata:
   name: default
 spec:
   # Use GH container registry to get rid of Docker limitation.
-  registry: ghcr.io
+  registry: ghcr.io/
   imagePath: flatcar/calico
   # Configures Calico networking.
   calicoNetwork:
