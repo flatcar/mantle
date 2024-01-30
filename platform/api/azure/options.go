9003caafbc9413e03749b418aba473fecf7cf1e7
Mathieu Tortuyaux <mtortuyaux@microsoft.com>
2024-01-30 11:49:55 +0100

    kola/options: add azure-resource-to-keep
    this can be helpful when we want to exclude from deletion a resource in
    a given resource group.
    
    Signed-off-by: Mathieu Tortuyaux <mtortuyaux@microsoft.com>

--- platform/api/azure/options.go
+++ platform/api/azure/options.go
@@ -55,4 +55,6 @@ type Options struct {
 	ResourceGroup string
 	// AvailabilitySetID is an existing availability set to deploy the instance in.
 	AvailabilitySetID string
+	// ResourceToKeep is a resource to keep when cleaning an existing ResourceGroup.
+	ResourceToKeep string
 }
