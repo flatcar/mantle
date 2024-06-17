5bdbfc69cdfcd62819d406306ba5081d5c9767ac
Mathieu Tortuyaux <mtortuyaux@microsoft.com>
2024-06-17 18:57:01 +0200

    kubeadm: test v1.30
    Signed-off-by: Mathieu Tortuyaux <mtortuyaux@microsoft.com>

--- kola/tests/kubeadm/kubeadm.go
+++ kola/tests/kubeadm/kubeadm.go
@@ -76,6 +76,19 @@ var (
 	// testConfig holds params for various kubernetes releases
 	// and the nested params are used to render script templates
 	testConfig = map[string]map[string]interface{}{
+		"v1.30.1": map[string]interface{}{
+			"HelmVersion":     "v3.13.2",
+			"MinMajorVersion": 3374,
+			// from https://github.com/flannel-io/flannel/releases
+			"FlannelVersion": "v0.22.0",
+			// from https://github.com/cilium/cilium/releases
+			"CiliumVersion": "1.12.5",
+			// from https://github.com/cilium/cilium-cli/releases
+			"CiliumCLIVersion": "v0.12.12",
+			"DownloadDir":      "/opt/bin",
+			"PodSubnet":        "192.168.0.0/17",
+			"cgroupv1":         false,
+		},
 		"v1.29.2": map[string]interface{}{
 			"HelmVersion":     "v3.13.2",
 			"MinMajorVersion": 3374,
